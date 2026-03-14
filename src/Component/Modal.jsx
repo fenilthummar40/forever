d856afb45e19fddafc22995def8096868d1a29af
fenil thummar <152170831+fenilthummar40@users.noreply.github.com>
2026-03-14 15:33:47 +0530

    Modal

--- src/Component/Modal.jsx
+++ src/Component/Modal.jsx
@@ -17,7 +17,7 @@ const Modal = ({isOpen, onClose, subscription}) => {
         try {
             const user = JSON.parse(localStorage.getItem("user"));
             const response = await axios.post(
-                "http://localhost:5000/api/subscriptionpayment/add",
+                "https://backend-uaa2.onrender.com/subscriptionpayment/add",
                 {
                     userid: user.id,
                     price: subscription.price,
